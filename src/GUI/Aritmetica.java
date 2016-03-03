b45094fd34e06420574972f624e4bb9c0b7d0dfc
Lucas Rodrigues <lucasrodriguesgui@hotmail.com>
2016-03-03 16:32:52 -0300

    Atualizado

--- src/GUI/Aritmetica.java
+++ src/GUI/Aritmetica.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Inspirio
@@ -10,6 +12,7 @@ public class Aritmetica extends javax.swing.JFrame {
      * Creates new form Aritmetica
      */
     public Aritmetica() {
+        this.operadores = new ArrayList<>();
         initComponents();
     }
 
@@ -41,7 +44,6 @@ public class Aritmetica extends javax.swing.JFrame {
         jButton15 = new javax.swing.JButton();
         jButton16 = new javax.swing.JButton();
         jButton17 = new javax.swing.JButton();
-        jButton18 = new javax.swing.JButton();
         jButton19 = new javax.swing.JButton();
         jButton20 = new javax.swing.JButton();
         jButton21 = new javax.swing.JButton();
@@ -58,26 +60,46 @@ public class Aritmetica extends javax.swing.JFrame {
         });
 
         jButton1.setText("+");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
 
         jButton2.setText("-");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
 
         jButton3.setText("*");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton3MouseClicked(evt);
             }
         });
 
         jButton4.setText("/");
+        jButton4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton4MouseClicked(evt);
+            }
+        });
 
         jButton5.setText("MOD");
-        jButton5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
+        jButton5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton5MouseClicked(evt);
             }
         });
 
         jButton6.setText("PI");
+        jButton6.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton6MouseClicked(evt);
+            }
+        });
         jButton6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton6ActionPerformed(evt);
@@ -85,6 +107,11 @@ public class Aritmetica extends javax.swing.JFrame {
         });
 
         jButton7.setText("SEN");
+        jButton7.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton7MouseClicked(evt);
+            }
+        });
 
         jButton8.setText("COS");
 
@@ -105,8 +132,6 @@ public class Aritmetica extends javax.swing.JFrame {
         jButton16.setText("X!");
 
         jButton17.setText("LN(X)");
-
-        jButton18.setText("%");
 
         jButton19.setText("=");
         jButton19.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -147,7 +172,7 @@ public class Aritmetica extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
                         .addComponent(jButton11))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton5)
+                        .addComponent(jButton6)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton7)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -160,8 +185,6 @@ public class Aritmetica extends javax.swing.JFrame {
                         .addComponent(jButton16)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton17)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton6)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton1)
@@ -172,7 +195,7 @@ public class Aritmetica extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton18)
+                        .addComponent(jButton5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton22)))
                 .addContainerGap())
@@ -190,15 +213,15 @@ public class Aritmetica extends javax.swing.JFrame {
                     .addComponent(jButton2)
                     .addComponent(jButton3)
                     .addComponent(jButton4)
-                    .addComponent(jButton18)
-                    .addComponent(jButton22))
+                    .addComponent(jButton22)
+                    .addComponent(jButton5))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton7)
                     .addComponent(jButton8)
                     .addComponent(jButton9)
-                    .addComponent(jButton5)
-                    .addComponent(jButton10))
+                    .addComponent(jButton10)
+                    .addComponent(jButton6))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton12)
@@ -209,8 +232,7 @@ public class Aritmetica extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton16)
-                    .addComponent(jButton17)
-                    .addComponent(jButton6))
+                    .addComponent(jButton17))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton19)
@@ -222,14 +244,6 @@ public class Aritmetica extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton5ActionPerformed
-
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton6ActionPerformed
@@ -239,13 +253,90 @@ public class Aritmetica extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jButton19MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton19MousePressed
-        equacao = jTextField2.getText();
+        
         //jTextField1.setText(equacao);
     }//GEN-LAST:event_jButton19MousePressed
 
-    /**
-     * @param args the command line arguments
-     */
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        operador = Double.parseDouble(jTextField2.getText());
+        operadores.add(operador);
+        
+        jTextField2.setText("");
+        
+        sinais.add("+");
+        
+        equacao.append(operador).append("+");
+        
+        jTextField1.setText(equacao.toString());           
+
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        operador = Double.parseDouble(jTextField2.getText());
+        operadores.add(operador);
+        
+        jTextField2.setText("");
+        
+        sinais.add("-");
+
+        equacao.append(operador).append("-");
+        
+        jTextField1.setText(equacao.toString());
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+        operador = Double.parseDouble(jTextField2.getText());
+        operadores.add(operador);
+        
+        jTextField2.setText("");
+        
+        sinais.add("*");
+        
+        equacao.append(operador).append("*");
+        
+        jTextField1.setText(equacao.toString());
+    }//GEN-LAST:event_jButton3MouseClicked
+
+    private void jButton4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton4MouseClicked
+        operador = Double.parseDouble(jTextField2.getText());
+        operadores.add(operador);
+        
+        jTextField2.setText("");
+        
+        sinais.add("/");
+        
+        equacao.append(operador).append("/");
+        
+        jTextField1.setText(equacao.toString());
+    }//GEN-LAST:event_jButton4MouseClicked
+
+    private void jButton5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton5MouseClicked
+        operador = Double.parseDouble(jTextField2.getText());
+        operadores.add(operador);
+        
+        jTextField2.setText("");
+        
+        sinais.add("%");
+        
+        equacao.append(operador).append("%");
+        
+        jTextField1.setText(equacao.toString());
+    }//GEN-LAST:event_jButton5MouseClicked
+
+    private void jButton6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton6MouseClicked
+        operadores.add(Math.PI);
+        
+        jTextField2.setText("");
+        
+        equacao.append(Math.PI);
+        
+        jTextField1.setText(equacao.toString());
+    }//GEN-LAST:event_jButton6MouseClicked
+
+    private void jButton7MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton7MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton7MouseClicked
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -274,8 +365,11 @@ public class Aritmetica extends javax.swing.JFrame {
         });
     }
     
+    private ArrayList<String> sinais = new ArrayList<>();
+    private ArrayList<Double> operadores = new ArrayList<>();
     private double resultado;
-    private String equacao;
+    private double operador;
+    private StringBuilder equacao = new StringBuilder();
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
@@ -287,7 +381,6 @@ public class Aritmetica extends javax.swing.JFrame {
     private javax.swing.JButton jButton15;
     private javax.swing.JButton jButton16;
     private javax.swing.JButton jButton17;
-    private javax.swing.JButton jButton18;
     private javax.swing.JButton jButton19;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton20;
--- src/calculadoracientifica/Calculadora.java
+++ src/calculadoracientifica/Calculadora.java
@@ -7,27 +7,9 @@ public class Calculadora
     
     public Calculadora(){}
     
-    public ArrayList<String> converter(String equacao){
-        ArrayList<String> equacaoFinal = new ArrayList<>();
-        for(int i=0;i<equacao.length();i++){
-            
-            equacaoFinal.add();
-        }
-        return equacaoFinal;
-    }
     
     public double interpretador(String equacao)
     {
-        ArrayList equacaoFinal;
-        equacaoFinal = converter(equacao);
-        
-        int c = 0;
-        for(int i=0;i<equacaoFinal.size();i++){
-            if(equacaoFinal.get(i)=="(")
-                do{
-                    
-                }while(equacaoFinal.get(i)!=")");
-        }
         
         return 2.0;
     }

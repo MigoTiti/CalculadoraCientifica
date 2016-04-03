480a4f4945611689ee177bcdccf2171a995a6edb
Lucas Rodrigues <lucasrodriguesgui@hotmail.com>
2016-04-03 04:23:46 -0300

    Added files via upload

---
+++ src/calculadoracientifica/Interfaces/MetodosPrimitivos.java
@@ -0,0 +1,22 @@
+package calculadoracientifica.Interfaces;
+
+public interface MetodosPrimitivos {
+    
+    public double soma(double a, double b);    
+    public double subtracao(double a, double b);
+    public double divisao(double a, double b);
+    public double multiplicacao(double a, double b);
+    public double potencia(double base, double expoente);
+    public double raizQuadrada(double a);
+    public double resto(double dividendo, double divisor);
+    public double log10(double a);
+    public double logNatural(double a);
+    public double seno(double a);
+    public double cosseno(double a);
+    public double tangente(double a);
+}
+
+
+
+
+
